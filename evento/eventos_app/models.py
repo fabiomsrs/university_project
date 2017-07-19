e18acce01414008196829202d57a5a3723237ac8
fabiano <fabiomsrs@gmail.com>
2017-07-18 23:02:38 -0300

    model p1

--- evento/eventos_app/models.py
+++ evento/eventos_app/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+	nome = models.charField(max_length=25)
+	login = models.charField(max_length=25)
+	password = models.charField(max_length=25)
 
-class 
+class Evento(models.Model):
+	usuario = models.ForeignKey('Usuario')	
+
+class Atividade(models.Model):
+	nome = models.charField(max_length=25)	
+	descricao = models.charField(max_length=25)
+	atividade = models.ForeignKey('Evento')
+
+class Inscricao(models.Model):
+	valor = models.IntegerField()
+
+class Cupom(models.Model):
+	desconto	 = models.IntegerField()
+	inscricao = models.ForeignKey('Inscricao')
