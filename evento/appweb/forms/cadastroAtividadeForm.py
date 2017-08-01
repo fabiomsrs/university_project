811f4097a8e3e270f04bd5cb85a9d4abbe0a78cb
fabiano <fabiomsrs@gmail.com>
2017-08-01 12:31:34 -0300

    form atividade

--- evento/appweb/forms/cadastroAtividadeForm.py
+++ evento/appweb/forms/cadastroAtividadeForm.py
@@ -1,7 +1,15 @@
 from django import forms
 from evento.models import Atividade
 
-class EventoForm(forms.ModelForm):
+class AtividadeForm(forms.ModelForm):
+	def __init__(self, *args, **kwargs):
+		self.request = kwargs.pop("request")
+		super(AtividadeForm, self).__init__(*args, **kwargs)		
+	
+		self.fields['evento'].queryset =  self.request.user.meus_eventos.all()
+		self.fields['evento'].empty_label = 'Empty'			
+		self.fields['evento'].label = 'Evento pertencente'
+
 	class Meta:
-		model = Atividade
-		fields = ('nome_atividade','evento','descricao')
+		model = Atividade		
+		fields = ('nome_atividade','evento','descricao')				
--- evento/appweb/urls.py
+++ evento/appweb/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
 	url(r'^$',views.index, name='login'),
 	url(r'^home/$',views.home, name='home'),	
-	url(r'^cadastroUsuario/$',views.cadastroUsuario, name='cadastroUsuario'),
+	url(r'^cadastroUsuario/$',views.cadastroUsuario, name='cadastro_usuario'),
+	url(r'^cadastroEvento/$',views.cadastroEvento, name='cadastro_evento'),
+	url(r'^cadastroAtividade/$',views.cadastroAtividade, name='cadastro_atividade'),
+	url(r'^$',views.logout, name='logout'),
 ]
