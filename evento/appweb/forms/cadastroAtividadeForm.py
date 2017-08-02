ff83cfa6f6742194f010d0320e7ef99c5cc4c802
fabiano <fabiomsrs@gmail.com>
2017-08-02 09:32:47 -0300

    form atividade issue

--- evento/appweb/forms/cadastroAtividadeForm.py
+++ evento/appweb/forms/cadastroAtividadeForm.py
@@ -3,10 +3,10 @@ from evento.models import Atividade
 
 class AtividadeForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
-		self.request = kwargs.pop("request")
+		self.user = kwargs.pop("user")
 		super(AtividadeForm, self).__init__(*args, **kwargs)		
 	
-		self.fields['evento'].queryset =  self.request.user.meus_eventos.all()
+		self.fields['evento'].queryset =  self.user.meus_eventos.all()
 		self.fields['evento'].empty_label = 'Empty'			
 		self.fields['evento'].label = 'Evento pertencente'
 
