8038a6a0f18856a7c53b7af75343174826d620df
fabiano <fabiomsrs@gmail.com>
2017-08-07 12:26:43 -0300

    changing function view to class based view

--- evento/evento/views.py
+++ evento/evento/views.py
@@ -1,46 +1,44 @@
 from django.shortcuts import render,redirect
-
+from django.views import View
 from appweb.forms.cadastroUsuarioForm import UsuarioForm
 from appweb.forms.cadastroEventoForm import EventoForm
 from appweb.forms.cadastroAtividadeForm import AtividadeForm
 from appweb.forms.cadastroCupomForm import CupomForm
 
-def cadastro_atividade(request):
-	user = request.user
-	if request.method == "POST":		
-		form = AtividadeForm(request.POST, user=user)		
+class CadastroAtividade(View):
+	form = AtividadeForm
+
+	def get(self, request, *args, **kwargs):
+		form = self.form(user=self.request.user)
+		return render(request, 'appweb/form.html', {'form': form})
+
+	def post(self, request, *args, **kwargs):		
+		form = self.form(request.POST, user=self.request.user)		
 		if form.is_valid():
 			atividade = form.save(commit = False) 								
 			atividade.save()
 			return redirect('home')			
-	else:		
-		form = AtividadeForm(user=user)
-		return render(request, 'appweb/form.html', {'form': form})
 
-def cadastro_cupom(request):
-	user = request.user
-	print(request.POST,"\n\n")
-	if request.method == "POST":				
-		form = CupomForm(request.POST, user=user)		
+class cadastroCupom(View):
+	def get(self, request, *args, **kwargs):		
+		form = CupomForm(user=self.request.user)
+		return render(request, 'appweb/cadastroCupom.html', {'form': form})
+
+	def post(self, request, *args, **kwargs):			
+		form = CupomForm(request.POST, user=self.request.user)		
 		if form.is_valid():
 			cupom = form.save(commit = False) 								
 			cupom.save()
-			return redirect('home')
-		else:
-			print('ERROOOOOOOR')
-			print(request.POST,"\n\n")				
-	else:		
-		form = CupomForm(user=user)
-		return render(request, 'appweb/cadastroCupom.html', {'form': form})
+			return redirect('home')		
 
-def cadastro_evento(request):
-	if request.method == "POST":
+class cadastroEvento(View):
+	def post(self, request, *args, **kwargs):	
 		form = EventoForm(request.POST)
 		if form.is_valid():
 			evento = form.save(commit = False) 					
 			evento.usuario_criador = request.user
 			evento.save()
 			return redirect('home')			
-	else:
+	def get(self, request, *args, **kwargs):
 		form = EventoForm()
 		return render(request, 'appweb/form.html', {'form': form})
