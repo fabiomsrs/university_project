a6ddc735af31153c272c92b48171202746cfdb0d
fabiano <fabiomsrs@gmail.com>
2017-08-12 04:01:24 -0300

    jquery inscricao

--- evento/inscricao/views.py
+++ evento/inscricao/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.views import View
+from appweb.forms.inscricaoForm import InscricaoForm
 # Create your views here.
+class Inscricao(View):
+	form = InscricaoForm
+
+	def post(self, request, *args, **kwargs):		
+		form = self.form(request.POST)		
+		if form.is_valid():
+			inscricao = form.save(commit = False) 								
+			inscricao.save()
+			return redirect('home')		
+
+	def get(self, request, *args, **kwargs):
+		form = self.form()		
+		return render(request, 'appweb/ajaxForm.html', {'form': form})		
