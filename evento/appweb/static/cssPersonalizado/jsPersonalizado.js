371c40459f174cc2ebaed362e1786fa2837ade9c
H1d3l <hidels.rios@gmail.com>
2017-09-01 16:03:30 -0300

    ajustado jquery

--- evento/appweb/static/cssPersonalizado/jsPersonalizado.js
+++
@@ -1,2 +0,0 @@
-$( document ).ready(function)
-$(".dropdown-button").dropdown({ hover: false });
---
+++ evento/appweb/static/jsPersonalizado.js
@@ -0,0 +1,6 @@
+
+$( document ).ready(function(){
+	//alert("Hidel is a sad boy");
+	$(".dropdown-button").dropdown();
+});
+
