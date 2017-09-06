41a947538d4b124dd16df1563924154023fdff8e
fabiano <fabiomsrs@gmail.com>
2017-09-06 01:36:38 -0300

    fixing cupom test

--- evento/inscricao/models.py
+++ evento/inscricao/models.py
@@ -41,10 +41,12 @@ class CupomAutomatico(Cupom):
 			e = EventoInscrevivel.objects.get(pk = self.evento.pk)
 			e.valor = e.valor * (1 - self.desconto/100)
 			e.save()			
+			return e.valor
 		else:
 			for atividade in Atividade.objects.filter(evento=self.evento):
 				atividade.valor = atividade.valor * (1 - self.desconto/100)
 				atividade.save()
+			return [atividade.valor for atividade in Atividade.objects.filter(evento=self.evento)]				
 
 class Pagamento(models.Model):	
 	valor_total = models.FloatField(null=True)
